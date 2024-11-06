4deedd1a7c88cd92ac1f0b47797f63033a0b2e8c
umar23-zan <umar.zangroups@gmail.com>
2024-11-06 10:35:50 +0530

    overall layout created

--- src/components/Career.js
+++ src/components/Career.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import backgroundvideo from '../images/careers-1.mp4';
+import './career.css'
 
 
 const Career = () => {
   return (
     <div>
+      <video src={backgroundvideo} loop autoPlay muted id='bg-video'></video>
       <Header />
-      career
+      <div className='career-info'>
+        <h1>Join the Band</h1>
+        <p>Career@zangroups.com</p>
+      </div>
       <Footer />
     </div>
   )
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import facebook from '../images/facebook.svg'
+import instagram from '../images/instagram.svg'
+import linkedin from '../images/linkedin.svg'
+import twitter from '../images/twitter.svg'
+import './contact.css'
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <Header />
+      <div className='contact-section'>
+          <div>
+            <h1>Contact us</h1>
+            <p>Feel free to contact us with any questions or concerns. You can use the form on our website or email us directly. We appreciate your interest and look forward to hearing from you.</p>
+            <h3>Email</h3>
+            <p>info@zangroups.com</p>
+            <h3>Address</h3>
+            <p>Alwarpet, Chennai.</p>
+          </div>
+          <div className='socialmedia-section'>
+              <div><p><strong>Follow us</strong></p></div>
+              <div className='socialmedia-icons'>
+                <img src={facebook} alt="facebook" />
+                <img src={instagram} alt="insta" />
+                <img src={linkedin} alt="linkedin" />
+                <img src={twitter} alt="twitter" />
+              </div>
+          </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
