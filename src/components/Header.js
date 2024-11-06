adb7bbb3ad06973a2c858108c69a16ba4e3193ad
umar23-zan <umar.zangroups@gmail.com>
2024-11-06 10:50:12 +0530

    header css inprovement

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import {  Link } from 'react-router-dom';
-import logo from '../images/Logo.png'
-import './header.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../images/Logo.png';
+import './header.css';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className='header-section'>
       <div className='header-logo'>
         <img src={logo} alt="logo" />
       </div>
       <div className='header-nav-section'>
-        <div className='header-navlinks'>
-        <Link to="/">Home</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/career">Career</Link>
+        <div className={`header-navlinks ${menuOpen ? 'open' : ''}`}>
+          <Link to="/" onClick={toggleMenu}>Home</Link>
+          <Link to="/services" onClick={toggleMenu}>Services</Link>
+          <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+          <Link to="/career" onClick={toggleMenu}>Career</Link>
+        </div>
+        <div className='hamburger' onClick={toggleMenu}>
+          ☰
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
