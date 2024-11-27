764900529c4880b73106d72f479dd2c609187b1a
umar23-zan <umar.zangroups@gmail.com>
2024-11-27 15:31:27 +0530

    logo changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../images/Logo.png';
+import logo from '../images/zanLogo.png';
 import './header.css';
 
 const Header = () => {
