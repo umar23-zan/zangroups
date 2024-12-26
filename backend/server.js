cc9a62a3e5c035338604bc885ed855aa0cb641a4
umar23-zan <umar.zangroups@gmail.com>
2024-12-26 12:53:29 +0530

    joined frontend to backend

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,12 @@ mongoose
 app.use(newsletterRoutes);
 app.use(contactRoutes);
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
